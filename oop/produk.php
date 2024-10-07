e0852bf694d516e72f2b3430763a14263e6899d4
Lyramor <mmarsa2435@gmail.com>
2024-10-08 03:08:34 +0700

    Video 3 - Property dan Method

--- oop/produk.php
+++ oop/produk.php
@@ -1,13 +1,40 @@
 <?php 
 
-// Jualan Produk
-// Komik
-// Game
-
 class Produk {
+  public $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0;
 
+  public function getLabel(){
+    return "$this->judul, $this->penulis";
+  }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk ();
+// $produk1 -> judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2 -> judul = "Uncharted";
+// $produk2 -> penulis = "Marsa";
+// var_dump($produk2)
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kisimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<hr>";
+
+echo "Game : " . $produk4->getLabel();
+
 ?>
