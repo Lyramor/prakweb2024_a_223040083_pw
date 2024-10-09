69a53005d8d40805d82331e8a3dab234cc4c5930
Lyramor <mmarsa2435@gmail.com>
2024-10-09 21:37:12 +0700

    video 12 - COnstant

---
+++ oop/constat.php
@@ -0,0 +1,32 @@
+<?php 
+
+// define('NAMA', 'Muhamad Marsa Nur Jaman');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+
+// class Coba {
+//   const NAMA = 'Marsa';
+// }
+
+// echo COBA::NAMA;
+
+class Coba {
+  public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+echo "<hr>";
+echo __LINE__;
+echo "<hr>";
+echo __FILE__;
+echo "<hr>";
+echo __DIR__;
+echo "<hr>";
+
+?>
